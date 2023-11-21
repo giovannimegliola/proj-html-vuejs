a6350a7707aaf61d46dc9855efcc3bbacb05aed1
giovannimegliola <giovanni.megliola@hotmail.it>
2023-11-21 17:10:40 +0100

    defined main content basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <HeaderApp /> 
-  <MainContentApp /> Main Content
+  <MainContentApp /> 
   <FooterApp />
 </template>
 
--- src/components/MainContentApp.vue
+++ src/components/MainContentApp.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
+  <div class="container">
+    <section id="hero">
+      Hero content
+    </section>
+    <section id="about">
+      About me
+    </section>
+    <section id="courses">
+      Online courses
+    </section>
+    <section id="books">
+      Book store
+    </section>
+    <section id="stories">
+     Real stories
+    </section>
+    <section id="blog">
+      Blog Updates
+    </section>
 
   </div>
 </template>
