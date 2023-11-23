a4200d903ca6a12d541dd65021e27bc81b5ae35d
giovannimegliola <giovanni.megliola@hotmail.it>
2023-11-23 16:53:44 +0100

    added dropdown menu in header

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -11,9 +11,19 @@
           <nav class="d-flex justify-content-between align-items-center">
             <ul class="nav">
               <li v-for="(link,index) in links" :key="index" class="nav-item">
-                <a v-bind:href="link.href" class="nav-link" :class="{ active: link.active }">
+                <a v-bind:href="link.href" class="nav-link" :class="{ active: link.active }" @mouseover="toggleDropdown(index)" @mouseleave="link.showDropdown = false">
                   {{ link.text }} <i class="fa-solid fa-angle-down myicon"></i>
                 </a>
+                <div v-if="link.showDropdown" class="dropdown-content">
+                  <ul>
+                    <li>MaxCoach education</li>
+                    <li>Course Portal</li>
+                    <li>Modern Schooling</li>
+                    <li>Health Coaching</li>
+                    <li>Business</li>
+                    <li>Courses Grid</li>
+                  </ul>
+                </div>
               </li>
             </ul>
           </nav>
@@ -37,7 +47,18 @@
       return{
         links
       };
-    }
+    },
+    methods: {
+    toggleDropdown(index) {
+      // Nascondi tutti i dropdown
+      this.links.forEach((link) => {
+        link.showDropdown = false;
+      });
+
+      // Mostra il dropdown del link cliccato
+      this.links[index].showDropdown = true;
+    },
+  },
   }
 </script>
 
@@ -68,5 +89,25 @@ i:hover{
 
 .myicon{
   font-size: 10px;
+}
+
+.dropdown-content {
+  display: block;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+.dropdown-content ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.dropdown-content li {
+  padding: 10px;
+  text-align: left;
 }
 </style>
