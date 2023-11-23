356af4b6c2dcf63f1a7e98771a4800b21070dd11
giovannimegliola <giovanni.megliola@hotmail.it>
2023-11-23 12:27:49 +0100

    added carousel in Stories component

--- src/components/StoriesApp.vue
+++ src/components/StoriesApp.vue
@@ -1,37 +1,92 @@
 <template>
   <section id="stories">
     <div class="row">
-      <div class="col-md-6 text-center ">
-        <h4>REAL STORIES</h4>
-        
-
+      <div class="col-md-6 text-center">
+        <h4 class="py-3">REAL STORIES</h4>
+        <div class="carousel">
+          <p class="mytext py-3 ">{{ currentStory.text }}</p>
+          <img :src="currentStory.image" :alt="currentStory.name"/>
+          <p class="py-2 myname">{{ currentStory.name }} </p> 
+          <p class="myrole">{{ currentStory.role }}</p>
+        </div>
+        <button @click="prevSlide" class="mybtn"> &#60; </button>
+        <button @click="nextSlide" class="mybtn"> &#62;</button>
       </div>
       <div class="col-md-6">
-        <img src="../assets/images/home-movation-testimonial-image-768x562.jpg" alt="">
-        
-
+        <img src="../assets/images/home-movation-testimonial-image-768x562.jpg" alt=""/>
       </div>
-
     </div>
-
   </section>
-  
 </template>
 
 <script>
-  export default {
-    name: 'StoriesApp'
-    
-  }
+import {stories} from "../data/stories.js"
+export default {
+  name: "StoriesApp",
+  data() {
+    return {
+      stories,
+      currentSlide: 0,
+    };
+  },  
+  computed: {
+    currentStory(){
+      return this.stories[this.currentSlide];
+    },
+  },
+  methods: {
+    prevSlide() {
+      this.currentSlide = (this.currentSlide - 1 + this.stories.length) % this.stories.length;
+    },
+    nextSlide() {
+      this.currentSlide = (this.currentSlide + 1) % this.stories.length;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-#stories{
+#stories {
   background-image: url(../assets/images/background-pattern-wavify.png);
 }
 
-h4{
+h4 {
   color: #20ad96;
 }
 
+.carousel {
+  position: relative;
+  width: 100%;
+  max-width: 400px; 
+  margin: 0 auto; 
+}
+
+.carousel img {
+  width: 90px;
+  height: auto;
+  border-radius: 50%;
+}
+
+button {
+  margin: 10px;
+}
+
+.mytext{
+  font-size: 27px;
+  font-weight: 200;
+}
+
+.myname{
+  font-size: 20px;
+}
+
+.myrole{
+  font-size: 15px;
+  font-weight: 200;
+}
+
+.mybtn{
+  border-radius: 50%;
+  border: none;
+}
 </style>
---
+++ src/data/stories.js
@@ -0,0 +1,28 @@
+const stories = [
+  {
+    "text":"I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.",
+    "image" : "/public/images/testimonial-avata-02.jpg",
+    "name":"Mina Hollace",
+    "role":"/Freelancer",
+  },
+  {
+    "text":"I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.",
+    "image" : "/public/images/testimonial-avata-04.jpg",
+    "name":"Madley Pondor",
+    "role":"/IT Specialist",
+  },
+  {
+    "text":"I am happy with their arrangement of lessons and subjects. They reflect a scientific investigation into effective methods to adopt for learners.",
+    "image" : "/public/images/testimonial-avata-01.jpg",
+    "name":"Luvic Dubble",
+    "role":"/Private Tutor",
+  },
+  {
+    "text":"I'm a very strict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just got me.",
+    "image" : "/public/images/testimonial-avata-03.jpg",
+    "name":"Florence Themes",
+    "role":"/Multimedia Admin",
+  },
+]
+
+export {stories};
